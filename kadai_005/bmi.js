a34dbbf316d86dc25d17823e21aa5be3857f835b
aitatsu-king <tat06tatta11@au.com>
2023-09-29 03:46:09 +0900

    first commit

---
+++ kadai_005/bmi.js
@@ -0,0 +1,8 @@
+let height = 1.81;
+let weight = 105;
+
+let bmi = Math.round(weight / (height ** 2) * 100) / 100;
+
+console.log("たつこさんの身長は" + height + "mです");
+console.log("たつこさんの体重は" + weight + "kgです");
+console.log("たつこさんのBMIは" + bmi + "です");
